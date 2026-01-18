76bf37d27f579fa2c9c8c0d78aae1958989d6037
sio2 <suho3938@gmail.com>
2026-01-18 23:21:50 +0900

    fix: 장르 목록 수정

--- app/src/main/java/com/mobile/soundscape/data/GenreDataFix.kt
+++ app/src/main/java/com/mobile/soundscape/data/GenreDataFix.kt
@@ -6,18 +6,18 @@ import com.mobile.soundscape.onboarding.GenreData
 object GenreDataFix {
     fun getGenreList(): MutableList<GenreData> {
         return mutableListOf(
+            GenreData("팝"),
             GenreData("케이팝"),
-            GenreData("아시안팝"),
-            GenreData("클래식"),
-            GenreData("재즈"),
-            GenreData("인디"),
-            GenreData("소울/R&B"),
-            GenreData("K-힙합"),
+            GenreData("밴드"),
             GenreData("힙합"),
+            GenreData("인디"),
+            GenreData("발라드"),
+            GenreData("재즈"),
+            GenreData("클래식"),
             GenreData("락"),
             GenreData("EDM"),
-            GenreData("발라드"),
-            GenreData("팝"),
+            GenreData("OST"),
+            GenreData("R&B"),
         )
     }
 }
